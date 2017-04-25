74b389eb28008b5e818859823221b987eb858f26
David Sellen Lima dos Santos <davidsellen@MacBook-Pro-de-David.local>
2017-04-24 23:47:40 -0300

    Initial Commit

---
+++ ShouldGreetWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShouldGreetWorld
+//
+//  Created by David Sellen Lima dos Santos on 24/04/17.
+//  Copyright © 2017 dscode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
